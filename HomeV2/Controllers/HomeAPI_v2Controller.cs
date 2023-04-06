2b60bdf66bec725f70eb215e27b9041238039253
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2023-04-06 12:12:44 +0530

    Add project files.

---
+++ HomeV2/Controllers/HomeAPI_v2Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ConsumeAPI.Controllers
+{
+    [Route("CallAPI")]
+    [ApiController]
+
+    public class HomeAPI_v2Controller : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> CallAPI()
+        {
+            //Call Get API
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7187/");
+                using (HttpResponseMessage response = await client.GetAsync("api/HomeAPI"))
+                {
+                    var reponseContent = response.Content.ReadAsStringAsync().Result;
+                    response.EnsureSuccessStatusCode();
+                    return Ok(reponseContent);
+
+                }
+
+            }
+
+        }
+    }
+}
