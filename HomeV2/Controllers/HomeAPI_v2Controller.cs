0a54e64a38db783d7d3110d6a9cd28bf55ab9805
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2023-04-07 14:59:24 +0530

    Add Post, Put, Delete in controller

--- HomeV2/Controllers/HomeAPI_v2Controller.cs
+++ HomeV2/Controllers/HomeAPI_v2Controller.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
-using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Net.Http;
+
+
 
 namespace ConsumeAPI.Controllers
 {
-    [Route("CallAPI")]
+    [Route("api/CallAPI")]
     [ApiController]
 
     public class HomeAPI_v2Controller : Controller
     {
-        [HttpPost]
-        public async Task<IActionResult> CallAPI()
+        [HttpGet]
+        public async Task<IActionResult> GetApI()
         {
+
             //Call Get API
             using (var client = new HttpClient())
             {
@@ -26,7 +30,97 @@ namespace ConsumeAPI.Controllers
                 }
 
             }
-
         }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAPI()
+        {
+            //Call post API
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7187/");
+                var postData = new
+                {
+                    Id = 0,
+                    Name = "Sky View",
+                    Sqft = 1000,
+                    Occupancy = 5
+                };
+
+
+
+                var content = new StringContent(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json");
+
+                using (HttpResponseMessage response = await client.PostAsync("api/HomeAPI", content))
+                {
+                    var reponseContent = response.Content.ReadAsStringAsync().Result;
+                    response.EnsureSuccessStatusCode();
+                    return Ok(reponseContent);
+                }
+
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> PutAPI()
+        {
+            //Call post API
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7187/");
+                var postData = new
+                {
+                    Id = 2,
+                    Name = "Sky View",
+                    Sqft = 1000,
+                    Occupancy = 5
+                };
+
+
+
+                var content = new StringContent(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json");
+
+                using (HttpResponseMessage response = await client.PutAsync("api/HomeAPI/2", content))
+                {
+                    var reponseContent = response.Content.ReadAsStringAsync().Result;
+                    response.EnsureSuccessStatusCode();
+                    return Ok(reponseContent);
+                }
+
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAPI()
+        {
+            //Call post API
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7187/");
+
+
+                using (HttpResponseMessage response = await client.DeleteAsync("api/HomeAPI/1"))
+                {
+                    var reponseContent = response.Content.ReadAsStringAsync().Result;
+                    response.EnsureSuccessStatusCode();
+                    return Ok(reponseContent);
+                }
+
+            }
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
